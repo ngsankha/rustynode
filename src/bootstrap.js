36d8e53cfd203e649d1d8e47f441b022b5c926b3
Sankha Narayan Guria <sankha93@gmail.com>
2015-11-28 03:53:39 +0530

    some more testing

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,10 +1,9 @@
 var timeoutRegistry = {};
 
 var setTimeout = function(callback, timeout) {
-  var timestamp = Date.now();
-  timeoutRegistry[timestamp] = callback;
-  _send(JSON.stringify({timestamp: timestamp, timeout: timeout}));
-  //_print(JSON.stringify({timestamp: timestamp, timeout: timeout}));
+  var id = Math.floor(Math.random() * 999999) + 1;
+  timeoutRegistry[id] = callback;
+  _send(JSON.stringify({timestamp: id, timeout: timeout}));
 }
 
 var _recv = function(message) {
@@ -17,5 +16,11 @@ var _recv = function(message) {
 
 setTimeout(function() {
   _print('World');
+  setTimeout(function() {
+    _print('I will always come later.');
+  }, 1000);
 }, 2000);
+setTimeout(function() {
+  _print('But I come first!');
+}, 1000);
 _print('Hello');
